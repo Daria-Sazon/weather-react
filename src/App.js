d5a8635ec9ee79036d8904297baead19b569f2e1
Daria <daria.sazonenko12@gmail.com>
2022-09-03 19:13:32 +0300

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Daria Sazon and is {" "}
+        <a href="https://github.com/Daria-Sazon/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
