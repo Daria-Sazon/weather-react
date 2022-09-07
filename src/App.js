3283642ea49b6ba07f08ba2935899a7ad2fc1ed7
Daria <daria.sazonenko12@gmail.com>
2022-09-07 19:25:58 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Daria Sazon and is {" "}
-        <a href="https://github.com/Daria-Sazon/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
-      </footer>
+      <div className='container'>
+
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Daria Sazon and is {" "}
+          <a href="https://github.com/Daria-Sazon/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
